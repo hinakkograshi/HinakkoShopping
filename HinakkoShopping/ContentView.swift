e2c896914ece6aeee1427c62e0c1707a7d6e80a7
hinakko <131275914+hinakko02@users.noreply.github.com>
2024-04-02 00:14:27 +0900

    add: first commit

---
+++ HinakkoShopping/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HinakkoShopping
+//
+//  Created by Hina on 2024/04/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HinakkoShopping/HinakkoShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HinakkoShoppingApp.swift
+//  HinakkoShopping
+//
+//  Created by Hina on 2024/04/02.
+//
+
+import SwiftUI
+
+@main
+struct HinakkoShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
