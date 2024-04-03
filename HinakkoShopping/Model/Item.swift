09f187359cd0437b8df005bde579fe35939ee916
hinakko <131275914+hinakko02@users.noreply.github.com>
2024-04-03 23:25:42 +0900

    add: DecodeModel

--- HinakkoShopping/Model/Item.swift
+++ HinakkoShopping/Model/Item.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+struct Item: Decodable {
+    let shoppingItem: [ShoppingItem]
+}
+
+struct ShoppingItem: Decodable, Identifiable {
+    let id = UUID()
+    let name: String
+    let category: String
+    let imageName: String
+
+    enum CodingKeys: String, CodingKey {
+        case name = "name"
+        case category = "category"
+        case imageName = "image_name"
+    }
+
+    init(from decoder: any Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        name = try values.decode(String.self, forKey: .name)
+        category = try values.decode(String.self, forKey: .category)
+        imageName = try values.decode(String.self, forKey: .imageName)
+    }
+}
